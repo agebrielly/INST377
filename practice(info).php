0bb4f0ad76242d0c525c4427bb1415563119201f
Amen <Amen@wireless-10-104-55-194.umd.edu>
2018-10-16 22:59:54 -0400

    Completion

---
+++ practice(info).php
@@ -0,0 +1,21 @@
+<?php
+class organization {
+  public $profile_id;
+  public $region;
+  public $Country_Income_Level;
+  public $Organization_Name;
+    public $Organization_Type;
+    public $Sectors;
+    public $Description;
+  public $City;
+  public $State;
+  public $Founding_Year;
+  public $Size;
+  public $Type_of_Data_Used;
+
+
+  function_Constructor($name, $year)   {
+  }
+public toJson()  {
+}
+?>
